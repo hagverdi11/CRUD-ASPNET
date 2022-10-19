a03005c46421b05bb2f1ab7f077665dd1bc49644
hagverdi11 <hagverdiam@code.edu.az>
2022-10-19 23:45:09 +0400

    https://github.com/hagverdi11/CRUD-ASPNET.git

---
+++ EntityFrameworkProject/Areas/AdminArea/Controllers/SocialController.cs
@@ -0,0 +1,59 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class SocialController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SocialController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Social> socials = await _context.Socials.ToListAsync();
+            return View(socials);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Social social)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+
+            bool isExist = await _context.Socials.AnyAsync(m => m.Name.Trim() == social.Name.Trim());
+
+
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", "Social media name already exist");
+                return View();
+            }
+
+            await _context.Socials.AddAsync(social);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+            return View();
+        }
+    }
+}
---
+++ EntityFrameworkProject/Models/Social.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Models
+{
+    public class Social : BaseEntity
+    {
+        [Required (ErrorMessage = "Name can not be empty")]
+        [StringLength(50, ErrorMessage ="Max size 50")]
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
